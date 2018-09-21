35619a4e8a985afa72a5750070d0411013799fbd
openec01 <33231497+openec01@users.noreply.github.com>
2018-09-21 17:10:34 +0100

    Update auto_script.sh

--- auto_script.sh
+++ auto_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-node /home/chukwuma/auto_archive_scripts/auto_bbchomepageLanding.js
-node /home/chukwuma/auto_archive_scripts/auto_bbciplayer.js
-node /home/chukwuma/auto_archive_scripts/auto_bbcnews.js
-/usr/local/bin/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /home/chukwuma/auto_archive_scripts/auto_archive_Timeout.js
-/usr/local/bin/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /home/chukwuma/auto_archive_scripts/auto_archive_noTimeout.js
+node /home/your_user/auto_archive_scripts/auto_bbchomepageLanding.js
+node /home/your_user/auto_archive_scripts/auto_bbciplayer.js
+node /home/your_user/auto_archive_scripts/auto_bbcnews.js
+/usr/local/bin/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /home/your_user/auto_archive_scripts/auto_archive_Timeout.js
+/usr/local/bin/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /home/your_user/auto_archive_scripts/auto_archive_noTimeout.js
 
